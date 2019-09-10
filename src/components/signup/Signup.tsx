388620882fb54b1ded113cdc349ebe0bdea88332
dummytree <dimitri@meldcx.com>
2019-09-10 21:46:53 +1000

    Temporary Commit
    - Will amend

--- src/components/signup/Signup.tsx
+++ src/components/signup/Signup.tsx
@@ -1,5 +1,5 @@
-import React, { Dispatch, SetStateAction } from 'react';
-import {Form, Col, Row, Button} from 'react-bootstrap';
+import React, { useState, Dispatch, SetStateAction } from 'react';
+import {Form, FormControl, InputGroup, Col, Row, Button} from 'react-bootstrap';
 import './Signup.test';
 import styled from 'styled-components/macro';
 
@@ -7,12 +7,44 @@ type SignupProps = {
 	setLoggedIn: Dispatch<SetStateAction<boolean>>;
 }
 
+type QuestionProps = {
+	questionCounter: number
+}
+
+type SignupData = {
+	username: string | null,
+	dateOfbirth: string | null,
+	accountNumber: string | null
+}
+
 const Container = styled.div`
 	display: flex;
+	flex-direction: column;
 	justify-content:center;
 	align-items: center;
 	margin: none;
 	height: 90vh;
+`;
+
+const ContainerInputGroup = styled.div`
+	display: flex;
+	width: 100%;
+	justify-content: center;
+	align-items: center;
+`;
+
+const StyledInputGroup = styled(InputGroup)`
+	display: flex;
+	flex-direction:column;
+	width: 50%;
+`;
+
+const ControlButtonGroup = styled.div`
+	display: flex;
+	justify-content: space-evenly;
+	width: 50%;
+
+
 `;
 
 const StyledForm = styled(Form)`
@@ -21,7 +53,7 @@ const StyledForm = styled(Form)`
 `
 
 const StyledButton = styled(Button)`
-	width: 100%
+	width: 15vw;
 `;
 
 const Login = styled(Button)`
@@ -43,70 +75,88 @@ const Login = styled(Button)`
 		color:blue;
 	}
 `;
+
+
+
+const DateOfBirthField: React.FC = ({}) => (
+	<InputGroup className="mb-3">
+		<InputGroup.Prepend>
+			<InputGroup.Text id="basic-addon2">DOB</InputGroup.Text>
+		</InputGroup.Prepend>
+		<FormControl
+			placeholder="DD / MM / YYYY"
+			aria-label="DOB"
+			aria-describedby="basic-addon1"
+		/>
+	</InputGroup>)
+
+const UserNameField: React.FC = ({}) => (
+	<InputGroup className="mb-3">
+		<InputGroup.Prepend>
+			<InputGroup.Text id="basic-addon1">@</InputGroup.Text>
+		</InputGroup.Prepend>
+		<FormControl
+			placeholder="Username"
+			aria-label="Username"
+			aria-describedby="basic-addon1"
+		/>
+	</InputGroup>)
+
+
+const MetaMaskIntegrator: React.FC = ({}) => {
+
+	// Is MetaMask installed ?
+
+	// Is MetaMask allowed ?
+
+	// What is the address ?
+
+	// Can the user sign a message to prove ownership ?
+
+	//
+
+
+	return (
+	<InputGroup className="mb-3">
+		<InputGroup.Prepend>
+			<InputGroup.Text id="basic-addon1">@</InputGroup.Text>
+		</InputGroup.Prepend>
+		<FormControl
+			placeholder="Username"
+			aria-label="Username"
+			aria-describedby="basic-addon1"
+		/>
+	</InputGroup>)
+}
+
+const SignupQuestions: React.FC<{questionCounter: number}> = ({questionCounter}:{questionCounter: number}) => {
+
+	switch(questionCounter) {
+		case(0):
+			return <StyledInputGroup>
+					<div style={{textAlign: "center", fontWeight: "bold", padding: "2%"}}> Create New Account</div>
+					<UserNameField/>
+					<DateOfBirthField/>
+				</StyledInputGroup>
+		case(1):
+			return <div>hello</div>
+		default:
+			return <MetaMaskIntegrator/>
+	}
+}
+
 export default ({setLoggedIn}: SignupProps) => {
+	const [questionCounter, setQuestionCounter] = useState(0)
+
 	return (
 		<Container>
-
-			<StyledForm>Customer Information
-				<Row>
-					<Col>
-						<Form.Group controlId = "formGroupFirstName">
-							<Form.Label>First Name</Form.Label>
-							<Form.Control type="text"></Form.Control>
-						</Form.Group>
-					</Col>
-					<Col>
-						<Form.Group controlId = "formGroupLastName">
-							<Form.Label>Last Name</Form.Label>
-							<Form.Control type="text"></Form.Control>
-						</Form.Group>
-					</Col>
-				</Row>
-
-				<Row>
-					<Col>
-						<Form.Group controlId = "formGroupEmail">
-							<Form.Label>Email</Form.Label>
-							<Form.Control type="text"></Form.Control>
-						</Form.Group>
-					</Col>
-					<Col>
-						<Form.Group controlId = "formGroupPhone">
-							<Form.Label>Phone</Form.Label>
-							<Form.Control type="text"></Form.Control>
-						</Form.Group>
-					</Col>
-				</Row>
-
-				<Row>
-					<Col>
-						<Form.Group controlId = "formGroupAddress">
-							<Form.Label>Address</Form.Label>
-							<Form.Control type="text"></Form.Control>
-						</Form.Group>
-					</Col>
-					<Col>
-						<Form.Group controlId = "formGroupDOB">
-							<Form.Label>Date of Birth</Form.Label>
-							<Form.Control type="text"></Form.Control>
-						</Form.Group>
-					</Col>
-				</Row>
-
-				<Row>
-					<Col>
-						<Form.Group controlId = "formGroupTFN">
-							<Form.Label>TFN</Form.Label>
-							<Form.Control type="text"></Form.Control>
-						</Form.Group>
-					</Col>
-				</Row>
-				<Row>
-					<StyledButton>Submit</StyledButton>
-					<Login>Already have an account? Login Here</Login>
-				</Row>
-			</StyledForm>
-
+			<ContainerInputGroup>
+				<SignupQuestions questionCounter={questionCounter}/>
+			</ContainerInputGroup>
+			<ControlButtonGroup>
+				<StyledButton onClick={()=>{setQuestionCounter((questionCounter + 1) % 3)}}>Back</StyledButton>
+				<StyledButton onClick={()=>{setQuestionCounter((questionCounter + 1) % 3)}}>Next</StyledButton>
+			</ControlButtonGroup>
 		</Container>
 	)
 }
